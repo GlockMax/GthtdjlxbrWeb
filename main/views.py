21784f1b486db73105ca29fec52e638d8dc1ef10
Glock Maxim <maksim.i.glock@gmail.com>
2020-07-31 10:33:26 +0500

    Создан веб переводчик

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def gthtdjlxbr(request):
+    return render(request, "main/gthtdjlxbr.html")
+
+def about(request):
+    return render(request, "main/about.html")
